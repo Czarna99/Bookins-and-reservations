267615c253a49fec722a6b79e0bca8000e36ed18
Paweł Joachimczuk <pawel@MacBook-Pro-Pawe.local>
2021-06-02 16:05:00 +0200

    sessions added

--- WebPage/cmd/web/main.go
+++ WebPage/cmd/web/main.go
@@ -7,13 +7,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
+
+var app config.AppConfig
+var session *scs.SessionManager
 
 const portNumber = ":8080"
 
 // main is the main function
 func main() {
-	var app config.AppConfig
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
